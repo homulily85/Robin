66cfc8b77e6a23718a21405a6eee6ef8efed217e
homulily85 <nguyenkimtrungduc2005@outlook.com>
2024-04-07 23:44:58 +0700

    Add animation for all objects in Level 1

--- state/level/level1.cpp
+++ state/level/level1.cpp
@@ -12,7 +12,7 @@
 
 constexpr int PLAYER_STRENGTH{ 22 };
 constexpr int ENEMY0_STRENGTH{ 15 };
-constexpr int ENEMY1_STRENGTH{ 15 };
+constexpr int ENEMY1_STRENGTH{ 30 };
 
 constexpr int PLAYER_BASE_X{ 296 };
 constexpr int PLAYER_BASE_Y{ 460 };
@@ -57,37 +57,50 @@ void Level1::update()
 		else return;
 	}
 	Vector2D* mouse_pos = Input_handle::instance()->get_mouse_position();
-	if (mouse_pos->get_x() > ENEMY0_BASE_X && mouse_pos->get_x() < ENEMY0_BASE_X+ BASE_WIDTH && mouse_pos->get_y() > ENEMY0_BASE_Y&& mouse_pos->get_y() < ENEMY0_BASE_Y+ BASE_HEIGHT &&Input_handle::instance()->get_mouse_state(LEFT)==true) {
+	if (mouse_pos->get_x() > ENEMY0_BASE_X && mouse_pos->get_x() < ENEMY0_BASE_X+ BASE_WIDTH && mouse_pos->get_y() > ENEMY0_BASE_Y&& mouse_pos->get_y() < ENEMY0_BASE_Y+ BASE_HEIGHT &&Input_handle::instance()->get_mouse_state(LEFT)==true&&m_enemy[0]->get_strength()>0&&m_player->get_strength()!=0) {
 		if (m_enemy[0] != nullptr) {
 			m_player->set_position(ENEMY0_BASE_X + 0.25*(BASE_WIDTH-PLAYER_WIDTH), ENEMY0_BASE_Y + 65);
 			if (m_player->get_strength() > m_enemy[0]->get_strength()) {
 				m_player->change_strength(m_enemy[0]->get_strength(), '+');
 				m_enemy[0]->set_strength_to_zero();
 				m_enemy[0]->change_texture("enemy_death"s, ENEMY_WIDTH, ENEMY_HEIGHT, SDL_FLIP_HORIZONTAL);
-				//m_player->change_texture("player_attack"s, PLAYER_WIDTH, PLAYER_HEIGHT, SDL_FLIP_NONE);
+				m_player->change_texture("player_attack"s, PLAYER_WIDTH, PLAYER_HEIGHT, SDL_FLIP_NONE);
 				m_player->set_attack(true);
 			}
-			else m_player->set_strength_to_zero();
+			else {
+				m_player->set_strength_to_zero();
+				m_player->change_texture("player_death"s, PLAYER_WIDTH, PLAYER_HEIGHT, SDL_FLIP_NONE);
+				m_enemy[0]->change_texture("enemy_attack"s, ENEMY_WIDTH, ENEMY_HEIGHT, SDL_FLIP_HORIZONTAL);
+				m_enemy[0]->set_attack(true);
+			}
 		}
 	}
-	else if (mouse_pos->get_x() > ENEMY1_BASE_X && mouse_pos->get_x() < ENEMY1_BASE_X+ BASE_WIDTH && mouse_pos->get_y() > ENEMY1_BASE_Y&& mouse_pos->get_y() < ENEMY1_BASE_Y+ BASE_HEIGHT && Input_handle::instance()->get_mouse_state(LEFT) == true) {
+	else if (mouse_pos->get_x() > ENEMY1_BASE_X && mouse_pos->get_x() < ENEMY1_BASE_X+ BASE_WIDTH && mouse_pos->get_y() > ENEMY1_BASE_Y&& mouse_pos->get_y() < ENEMY1_BASE_Y+ BASE_HEIGHT && Input_handle::instance()->get_mouse_state(LEFT) == true&&m_enemy[1]->get_strength()>0&&m_player->get_strength()!=0) {
 			if (m_enemy[1] != nullptr) {
 				m_player->set_position(ENEMY1_BASE_X+ 0.25 * (BASE_WIDTH - PLAYER_WIDTH), ENEMY1_BASE_Y+65);
 				if (m_player->get_strength() > m_enemy[1]->get_strength()) {
 					m_player->change_strength(m_enemy[1]->get_strength(), '+');
 					m_enemy[1]->set_strength_to_zero();
 					m_enemy[1]->change_texture("enemy_death"s, ENEMY_WIDTH, ENEMY_HEIGHT, SDL_FLIP_HORIZONTAL);
-					//m_player->change_texture("player_attack"s, PLAYER_WIDTH, PLAYER_HEIGHT, SDL_FLIP_NONE);
+					m_player->change_texture("player_attack"s, PLAYER_WIDTH, PLAYER_HEIGHT, SDL_FLIP_NONE);
 					m_player->set_attack(true);
 				}
-				else m_player->set_strength_to_zero();
+				else {
+					m_player->set_strength_to_zero();
+					m_player->change_texture("player_death"s, PLAYER_WIDTH, PLAYER_HEIGHT, SDL_FLIP_NONE);
+					m_enemy[1]->change_texture("enemy_attack"s, ENEMY_WIDTH, ENEMY_HEIGHT, SDL_FLIP_HORIZONTAL);
+					m_enemy[1]->set_attack(true);
+				}
 			}
 		}
-	/*if (m_player->get_strength() == 0) game::instance()->get_game_state_manager()->replace(new Game_over);
+	/*if (m_player->get_strength() == 0&&enemy_frame_check()) {
+		SDL_Delay(500);
+		game::instance()->get_game_state_manager()->replace(new Game_over);
+	}
 	else if (enemy_count == count_defeated() && enemy_frame_check()) {
 		SDL_Delay(500);
-		game::instance()->get_game_state_manager()->replace(new Victory);
-	}*/
+		game::instance()->get_game_state_manager()->replace(new Victory);*/
+	}
 }
 
 void Level1::render()
@@ -105,52 +118,83 @@ void Level1::render()
 		if (m_player->get_strength() != 0&&!m_player->get_attack()) {
 			m_player->set_current_frame((int)((SDL_GetTicks() / 100) % 11));
 		}
-		/*else if (m_player->get_strength() != 0 && m_player->get_attack()){
-			if (m_player->get_current_frame() < 3) {
-				int frame = m_enemy[0]->get_current_frame();
+		else if (m_player->get_strength() != 0 && m_player->get_attack()){
+			if (m_player->get_current_frame() < 2) {
+				int frame = m_player->get_current_frame();
 				frame_time = SDL_GetTicks() - start_time;
-				start_time = SDL_GetTicks();
 				if (frame_time > 100) {
 					frame++;
 					m_player->set_current_frame(frame);
-					SDL_Delay(50);
+					SDL_Delay(100);
 				}
 			}
 			else {
 				m_player->set_attack(false);
 				m_player->change_texture("player_idle"s, PLAYER_WIDTH, PLAYER_HEIGHT, SDL_FLIP_NONE);
 			}
-		}*/
+		}
+		else if (m_player->get_strength() == 0 && m_player->get_current_frame() <3 && !m_enemy[0]->get_attack() && !m_enemy[1]->get_attack()) {
+			int frame = m_player->get_current_frame();
+			frame_time = SDL_GetTicks() - start_time;
+			start_time = SDL_GetTicks();
+			if (frame_time > 100) {
+				frame++;
+				m_player->set_current_frame(frame);
+				SDL_Delay(100);
+			}
+		}
 	}
 	else return;
 	if (!m_exit && m_enemy[0] != nullptr) {
 		m_enemy[0]->draw();
 		m_enemy[0]->strength_to_text(ENEMY0_BASE_X+50, ENEMY0_BASE_Y+15);
-		if (m_enemy[0]->get_strength() == 0 && m_enemy[0]->get_current_frame() != 4) {
+		if (m_enemy[0]->get_strength() == 0 && m_enemy[0]->get_current_frame() != 4&&!m_player->get_attack()) {
 			int frame = m_enemy[0]->get_current_frame();
 			frame_time = SDL_GetTicks() - start_time;
 			start_time = SDL_GetTicks();
 			if (frame_time > 100) {
 				frame++;
 				m_enemy[0]->set_current_frame(frame);
-				SDL_Delay(50);
+				SDL_Delay(100);
 			}
 		}
+		else if (m_enemy[0]->get_attack() && m_enemy[0]->get_current_frame() < 7) {
+			int frame = m_enemy[0]->get_current_frame();
+			frame_time = SDL_GetTicks() - start_time;
+			start_time = SDL_GetTicks();
+			if (frame_time > 100) {
+				frame++;
+				m_enemy[0]->set_current_frame(frame);
+				SDL_Delay(100);
+			}
+		}
+		else m_enemy[0]->set_attack(false);
 	}
 	else return;
 	if (!m_exit && m_enemy[1] != nullptr) {
 		m_enemy[1]->draw();
 		m_enemy[1]->strength_to_text(ENEMY1_BASE_X+50, ENEMY1_BASE_Y+15);
-		if (m_enemy[1]->get_strength() == 0&&m_enemy[1]->get_current_frame()!=4) {
+		if (m_enemy[1]->get_strength() == 0&&m_enemy[1]->get_current_frame()!=4&&!m_player->get_attack()) {
 			int frame = m_enemy[1]->get_current_frame();
 			frame_time = SDL_GetTicks() - start_time;
 			start_time = SDL_GetTicks();
 			if (frame_time >100) {
 				frame++;
 				m_enemy[1]->set_current_frame(frame);
-				SDL_Delay(50);
+				SDL_Delay(100);
 			}
 		}
+		else if (m_enemy[1]->get_attack() && m_enemy[1]->get_current_frame() < 7) {
+			int frame = m_enemy[1]->get_current_frame();
+			frame_time = SDL_GetTicks() - start_time;
+			start_time = SDL_GetTicks();
+			if (frame_time > 100) {
+				frame++;
+				m_enemy[1]->set_current_frame(frame);
+				SDL_Delay(100);
+			}
+		}
+		else m_enemy[1]->set_attack(false);
 	}
 	else return;
 }
@@ -164,8 +208,10 @@ bool Level1::on_start()
 	if (!Texture_manager::instance()->load("pic/roof.png"s, "roof"s, game::instance()->get_renderer())) return false;
 	if (!Texture_manager::instance()->load("pic/player_idle.png"s, "player_idle"s, game::instance()->get_renderer())) return false;
 	if (!Texture_manager::instance()->load("pic/player_attack.png"s, "player_attack"s, game::instance()->get_renderer())) return false;
+	if (!Texture_manager::instance()->load("pic/player_death.png"s, "player_death"s, game::instance()->get_renderer())) return false;
 	if (!Texture_manager::instance()->load("pic/enemy_default.png"s, "enemy_default"s, game::instance()->get_renderer())) return false;
 	if (!Texture_manager::instance()->load("pic/enemy_death.png"s, "enemy_death"s, game::instance()->get_renderer())) return false;
+	if (!Texture_manager::instance()->load("pic/enemy_attack.png"s, "enemy_attack"s, game::instance()->get_renderer())) return false;
 
 	m_object.push_back(new Game_object("game_play_background"s, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT));
 	m_object.push_back(new Menu_button("replay"s, WINDOW_WIDTH*0.02, WINDOW_HEIGHT*0.02, REPLAY_WIDTH, REPLAY_HEIGHT, replay));
@@ -194,6 +240,8 @@ bool Level1::on_exit()
 	Texture_manager::instance()->remove_from_texture_map("enemy_default"s);
 	Texture_manager::instance()->remove_from_texture_map("enemy_death"s);
 	Texture_manager::instance()->remove_from_texture_map("player_attack"s);
+	Texture_manager::instance()->remove_from_texture_map("player_death"s);
+	Texture_manager::instance()->remove_from_texture_map("enemy_attack"s);
 	m_exit = true;
 	return true;
 }
@@ -222,8 +270,15 @@ int Level1::count_defeated()
 
 bool Level1::enemy_frame_check()
 {
-	for (auto p : m_enemy) {
-		if (p->get_current_frame() != 4) return false;
+	if (count_defeated() == enemy_count) {
+		for (auto p : m_enemy) {
+			if (p->get_current_frame() != 4) return false;
+		}
+		return true;
 	}
-	return true;
+	else if (m_player->get_strength() == 0) {
+		for (auto p : m_enemy) {
+			if (p->get_current_frame() == 8) return false;
+		}
+	}
 }
